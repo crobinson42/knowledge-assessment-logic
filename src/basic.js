c06697e9d267c60693751280e4f16b232ab63d98
Cory Robinson <coryrobinson42@gmail.com>
2016-09-03 20:15:48 -0700

    add initial

---
+++ src/basic.js
@@ -0,0 +1,95 @@
+module.exports = {
+  0: {
+    question: 'What number in the group?',
+    answers: {
+      0: 7,
+      1: 11,
+      2: 24,
+      3: 195,
+      4: 3
+    },
+    answer: 2
+  },
+  1: {
+    question: 'A farmer has 17 sheep and all but 9 die. How many sheep are left?',
+    answers: {
+      0: 0,
+      1: 8,
+      2: 9
+    },
+    answer: 2
+  },
+  2: {
+    question: 'You are in a dark room. You have a match, lamp, newspaper, & candle. Which do you light first?',
+    answers: {
+      0: 'candle',
+      1: 'newspaper',
+      2: 'match',
+      3: 'lamp'
+    },
+    answer: 2
+  },
+  3: {
+    question: 'Which weighs more, a pound of sand or a pound of feathers?',
+    answers: {
+      0: 'a pound of sand',
+      1: 'they are the same',
+      2: 'a pound of feathers'
+    },
+    answer: 1
+  },
+  4: {
+    question: 'If you have to be at work by 6pm and it takes you 23 minutes to drive to work, how late will you be if you left at 5:40am?',
+    answers: {
+      0: '3 minutes',
+      1: 'I will not be late',
+      2: '13 minutes'
+    },
+    answer: 1
+  },
+  5: {
+    question: 'What is the best order to get dressed if you can not put your pants on over your shoes?',
+    answers: {
+      0: 'Put your shirt on, then socks, then shoes, and finally pants.',
+      1: 'Put on pants, shoes, shirt, then socks.',
+      2: 'Socks, pants, shoes, then shirt.'
+    },
+    answer: 2
+  },
+  6: {
+    question: 'If yesterday was Monday, what day was it 3 days ago?',
+    answers: {
+      0: 'Friday',
+      1: 'Saturday',
+      2: 'Sunday'
+    },
+    answer: 1
+  },
+  7: {
+    question: 'Complete the pattern: 0, 4, 8, [?]',
+    answers: {
+      0: 10,
+      1: 12,
+      2: 14
+    },
+    answer: 1
+  },
+  8: {
+    question: 'In 24hr (military) time, what time is 7:30pm?',
+    answers: {
+      0: '1730 hr',
+      1: '1930 hr',
+      2: '0730 hr'
+    },
+    answer: 1
+  },
+  9: {
+    question: 'If all ducks quack and Donald is a duck, then...',
+    answers: {
+      0: 'Donald does not quack',
+      1: 'Donald has nothing to do with this',
+      2: 'Donald quacks'
+    },
+    answer: 2
+  }
+}
